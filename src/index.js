77032f096f5b87f8dc60be5d80061895c648ceb1
felipe <felipe.dreis.monteiro@gmail.com>
2023-01-03 11:07:36 -0300

    feat: project initilized

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`server running on port:${port}`));
