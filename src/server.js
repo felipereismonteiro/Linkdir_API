32152705845712be9585ed210c48d7ceef92649e
felipe <felipe.dreis.monteiro@gmail.com>
2023-01-03 12:34:58 -0300

    doc: all files

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { router } from "./routes/index.js"
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`server running on port:${port}`));
