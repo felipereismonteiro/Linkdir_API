07abf05e58f6c20afda93e74d4dc3761178e8a28
Rayandson <rayandson.silva321@gmail.com>
2023-01-04 17:15:29 -0300

    feat: get posts

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,4 +1,3 @@
-import { connectionDB } from "../db/db.js";
 import postsRepository from "../repositories/postsRepository.js";
 
 export async function createPost(req, res) {
@@ -6,8 +5,19 @@ export async function createPost(req, res) {
     const {user_id, content, url} = req.body;
 
     try {
-        await postsRepository.createUser(user_id, content, url);
+        await postsRepository.createPost(user_id, content, url);
         res.sendStatus(201);
+    } catch(err) {
+        res.status(500).send(err.message)
+    }
+    
+}
+
+export async function getPosts(req, res) {
+
+    try {
+        const posts = await postsRepository.getPosts();
+        res.status(200).send(posts.rows);
     } catch(err) {
         res.status(500).send(err.message)
     }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createPost } from "../controllers/postsControllers.js";
+import { createPost, getPosts } from "../controllers/postsControllers.js";
 import { validatePostSchema } from "../middlewares/postsMiddlewares.js";
 
 export const postsRouter = Router();
 
 postsRouter.post("/posts", validatePostSchema, createPost);
+postsRouter.get("/posts", getPosts);
