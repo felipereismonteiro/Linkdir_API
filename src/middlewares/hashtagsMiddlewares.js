1bda3c96fb50750aa4e83a49234beced12a2062e
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-01-05 15:27:24 -0300

    feat: add hashtag existence validation function

--- src/middlewares/hashtagsMiddlewares.js
+++ src/middlewares/hashtagsMiddlewares.js
@@ -2,29 +2,41 @@ import hashtagsRepository from "../repositories/hashtagsRepository.js";
 
 export async function hashtagAlreadyRegisteredValidation(req, res, next) {
   const { content } = req.body;
-  const {name} = req.params
-  console.log("validation")
+
   try {
-    if (req.method === "POST") {
+    const hashtags = hashtagsRepository.filterHashtags(content);
 
-      console.log("POOST")
-      const hashtags = hashtagsRepository.filterHashtags(content);
+    res.locals.hashtags = hashtags;
 
-      res.locals.hashtags = hashtags;
-
-      if (hashtags.length === 0) {
-        return next();
-      }
-
-      const { rows } = await hashtagsRepository.getHashtagsByNames(hashtags);
-
-      if (rows.length !== 0) {
-        res.locals.existingHashtags = rows;
-      }
-
-      next();
+    if (hashtags.length === 0) {
+      return next();
     }
-  
+
+    const { rows } = await hashtagsRepository.getHashtagsByNames(hashtags);
+
+    if (rows.length !== 0) {
+      res.locals.existingHashtags = rows;
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function hashtagExistenceValidation(req, res, next) {
+  const { hashtag } = req.params;
+
+  try {
+    const { rows } = await hashtagsRepository.getOneHashTagByName(hashtag);
+
+    if (rows.length === 0) {
+      return res.status(404).send({ message: "Hashtag not found" });
+    }
+
+    res.locals.hashtagId = rows[0].id;
+
+    return next();
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/routes/hashtagsRoutes.js
+++ src/routes/hashtagsRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { getHashtags } from "../controllers/hashtagsControllers.js";
+import { hashtagExistenceValidation } from "../middlewares/hashtagsMiddlewares.js";
 
 export const hashtagsRouter = Router();
 
 hashtagsRouter.get("/hashtags", getHashtags);
-hashtagsRouter.get("/hashtags/:hashtag");
+hashtagsRouter.get("/hashtags/:hashtag", hashtagExistenceValidation);
