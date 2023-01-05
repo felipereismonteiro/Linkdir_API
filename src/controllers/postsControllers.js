08c2af30cc877552e629511dabbd96b4ba4fc3d0
Rayandson <rayandson.silva321@gmail.com>
2023-01-05 13:42:53 -0300

    feat: get metadata

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -2,6 +2,7 @@ import hashtagsRepository, {
   postHashTagsAndPostIds,
 } from "../repositories/hashtagsRepository.js";
 import postsRepository from "../repositories/postsRepository.js";
+import urlMetadata from "url-metadata";
 
 export async function createPost(req, res) {
   const { user_id, content, url } = req.body;
@@ -41,9 +42,30 @@ export async function createPost(req, res) {
 
 export async function getPosts(req, res) {
 
+  // urlMetadata("https://www.youtube.com/watch?v=LwJvZOSQWrE&ab_channel=ESPNBrasil").then(metadata => {
+  //   console.log(metadata.title);
+  //   console.log(metadata.description);
+  //   console.log(metadata.image);
+  // });
+
     try {
-        const posts = await postsRepository.getPosts();
-        res.status(200).send(posts.rows);
+        const { rows } = await postsRepository.getPosts();
+        const postsPromises = rows.map(async (p) => {
+          let post = {};
+          await urlMetadata(p.url).then(metadata => {
+              if(!metadata.title) {
+                post = {...p, title: "Link", description: metadata.description, image: metadata.image}
+              } else {
+                post = {...p, title: metadata.title, description: metadata.description, image: metadata.image}
+              }
+   
+          });
+          
+          return post;
+        })
+        const posts = await Promise.all(postsPromises) 
+
+        res.status(200).send(posts); 
     } catch(err) {
         res.status(500).send(err.message) 
     }
