d90000d4225924ff9aed5340775c0c49ecb3312f
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2023-01-11 14:39:54 -0300

    feat: add follow insertion

--- src/controllers/followControllers.js
+++ src/controllers/followControllers.js
@@ -1,14 +1,15 @@
 import followRespository from "../repositories/followRepository.js";
 
-export async function followUser(req, res){
+export async function followUser(req, res) {
   const userId = res.locals.userId;
-  const {userToFollowId} = req.params;
+  const { userToFollowId } = req.params;
 
-  try{
+  try {
     await followRespository.followUser(userId, userToFollowId);
-    
-    res.sensStatus(200)
-  }catch(err){
-    res.status(500).send(err.message)
+
+    res.sensStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 }
+
